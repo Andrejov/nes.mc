273962b9a73efb4b2c815fec7d49a597b5f2f576
_Andrejov <ziomek0012@gmail.com>
2021-08-18 17:54:28 +0200

    Major changes - Scoreboard R4 setup & config - PlaceholderAPI setup & config w/ expansions - TAB config update w/ PAPI - windows test script -Xmx 16G -> 8G

--- scripts/test.bat
+++ scripts/test.bat
@@ -2,6 +2,6 @@
 
 CD ./DIST
 
-"%ProgramFiles%\Java\jdk-16.0.2\bin\java.exe" -Xms512M -Xmx16G -jar server.jar nogui
+"%ProgramFiles%\Java\jdk-16.0.2\bin\java.exe" -Xms512M -Xmx8G -jar server.jar nogui
 
 CD ../
